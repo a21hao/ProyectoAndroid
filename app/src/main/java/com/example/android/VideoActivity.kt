a616a822bfb0ab6ff94f92df181384fca14eeda2
Haojie Lin <94469307+a21hao@users.noreply.github.com>
2024-04-28 16:49:27 +0200

    permisos resuelto

--- app/src/main/java/com/example/android/VideoActivity.kt
+++ app/src/main/java/com/example/android/VideoActivity.kt
@@ -1,16 +1,22 @@
 package com.example.android
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class VideoActivity : AppCompatActivity() {
 
     private val REQUEST_VIDEO_CAPTURE = 101
     private val PERMISSION_REQUEST_CODE = 100
+    private val CAMERA_PERMISSION = Manifest.permission.CAMERA
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,9 +26,13 @@ class VideoActivity : AppCompatActivity() {
         val btnStopRecording = findViewById<Button>(R.id.btn_stop_recording)
 
         btnStartRecording.setOnClickListener {
-            dispatchTakeVideoIntent()
-            btnStartRecording.isEnabled = false
-            btnStopRecording.visibility = Button.VISIBLE
+            if (checkCameraPermission()) {
+                dispatchTakeVideoIntent()
+                btnStartRecording.isEnabled = false
+                btnStopRecording.visibility = Button.VISIBLE
+            } else {
+                requestCameraPermission()
+            }
         }
 
         btnStopRecording.setOnClickListener {
@@ -31,10 +41,56 @@ class VideoActivity : AppCompatActivity() {
         }
     }
 
+    private fun checkCameraPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            CAMERA_PERMISSION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestCameraPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(CAMERA_PERMISSION),
+            PERMISSION_REQUEST_CODE
+        )
+    }
+
     private fun dispatchTakeVideoIntent() {
         val takeVideoIntent = Intent(MediaStore.ACTION_VIDEO_CAPTURE)
         if (takeVideoIntent.resolveActivity(packageManager) != null) {
             startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array
+        <String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permiso concedido, puedes iniciar la grabación
+                    dispatchTakeVideoIntent()
+                } else {
+                    // Permiso denegado, muestra un mensaje o realiza alguna acción adicional si es necesario
+                    // Por ejemplo, puedes mostrar un mensaje al usuario indicando que necesitas el permiso de cámara para grabar video.
+                    // También podrías deshabilitar la funcionalidad de grabación de video o cerrar la actividad.
+                    // Aquí un ejemplo de cómo mostrar un mensaje:
+                    Toast.makeText(
+                        this,
+                        "Permiso de cámara necesario para grabar video",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+                return
+            }
+            else -> {
+                // Ignore all other requests.
+            }
         }
     }
 
